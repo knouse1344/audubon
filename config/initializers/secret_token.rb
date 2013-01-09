7d02d8b92dd70efe4b531617febcf2f018850efe
Bryan Knouse <bryan.knouse@gmail.com>
2013-01-09 10:58:46 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Audubon::Application.config.secret_token = 'e0a34678ed9f1686c1efde93e1e8c27de8c81fe4d69c79c20ebb089ea237c1a2e033604c4a9b7d43878b3ecec7941169f3d95873a881a96a06ec5fc5ec3a8779'
